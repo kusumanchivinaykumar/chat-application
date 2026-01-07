47c25819784a7358b36f641a14d627d16d7acfd7
kusumanchivinaykumar <vinaykusumanchi43@gmail.com>
2026-01-07 12:42:21 +0530

    Updated backend URL and fixed CORS configuration

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:5174","http://localhost:5174", "https://chat-application-client.netlify.app"],
     credentials: true,
   })
 );
